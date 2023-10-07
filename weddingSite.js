db44aa19c4b10a6aff27cc856d89074f16ed5814
Ryan Shouse <ryan.shouse@mykenzie.snhu.edu>
2023-10-07 14:01:48 -0500

    added embedded google form and made changes

--- weddingSite.js
+++ weddingSite.js
@@ -1,30 +1,12 @@
 const openRsvp = document.getElementById("openRsvp");
-const formSubmit = document.getElementById("formSubmit");
-const returnHome = document.getElementById("returnHome");
 const formWrapper = document.getElementById("formWrapper");
 const main = document.getElementById("main");
-const formSubmitted = document.getElementById("formSubmitted");
 const fireworks = document.getElementById("fireworks");
 
 openRsvp.addEventListener("click", function() {
     if (formWrapper.style.display = "none") {
-        formWrapper.style.display = "block"
-        main.style.display = "none";
-    }
-})
-
-formSubmit.addEventListener("click", function() {
-    if (formWrapper.style.display = "block") {
-        formWrapper.style.display = "none";
-        formSubmitted.style.display = "block";
+        formWrapper.style.display = "block";
         fireworks.style.display = "block";
-    }
-})
-
-returnHome.addEventListener("click", function() {
-    if (formSubmitted.style.display = "block") {
-        formSubmitted.style.display = "none";
-        fireworks.style.display = "none";
-        main.style.display = "block";
+        main.style.display = "none";
     }
 })
