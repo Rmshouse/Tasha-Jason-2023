ec82d2d32fbef2b760a240acf66b252118fcf124
Ryan Shouse <ryan.shouse@mykenzie.snhu.edu>
2023-10-07 14:12:59 -0500

    firework animation moved to main

--- weddingSite.js
+++ weddingSite.js
@@ -1,12 +1,10 @@
 const openRsvp = document.getElementById("openRsvp");
 const formWrapper = document.getElementById("formWrapper");
 const main = document.getElementById("main");
-const fireworks = document.getElementById("fireworks");
 
 openRsvp.addEventListener("click", function() {
     if (formWrapper.style.display = "none") {
         formWrapper.style.display = "block";
-        fireworks.style.display = "block";
         main.style.display = "none";
     }
 })
